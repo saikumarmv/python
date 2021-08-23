8b825cdd610d6fc66a3eda7dc9332d94024b8042
saikumarmv <saikumarmv977@gmail.com>
2021-08-23 17:57:17 +0530

    git commit

---
+++ sorting.py
@@ -0,0 +1,8 @@
+pagram = "The quick brown for jumps over the lazy dog"
+letter = sorted(pagram)
+print(letter)
+numbers = [1.2,9.5,6.3,3.6,5.4]
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+print(numbers)
+
